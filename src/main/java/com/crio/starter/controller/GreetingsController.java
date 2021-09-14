f249b4cacb2e1ceb276c976f0240e89cb1566aac
CrioUser <crio-user@criodo.com>
2021-09-14 13:40:44 +0530

    endpoint /memes/{id}

--- src/main/java/com/crio/starter/controller/GreetingsController.java
+++ src/main/java/com/crio/starter/controller/GreetingsController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,10 +38,20 @@ public class GreetingsController {
 
     } else if(greetingsService.findByName(name) != null || greetingsService.findByUrl(url) != null || greetingsService.findByCaption(caption) != null) {
       return ResponseEntity.status(409).body(null);
-      
+
     } else {
       ResponseDto response = greetingsService.postMeme(greetingsEntity);
       return ResponseEntity.ok().body(response);
+    }
+  }
+
+  @GetMapping("/memes/{id}")
+  public ResponseEntity<GreetingsEntity> getMeme(@PathVariable("id") Long id) {
+    GreetingsEntity greetingsEntity = greetingsService.findById(id);
+    if(greetingsEntity != null) {
+    return ResponseEntity.ok(greetingsEntity);
+    } else {
+    return ResponseEntity.ok(null);
     }
   }
 
