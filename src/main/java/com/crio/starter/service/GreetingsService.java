73410895c2038625e18305b5f3e7e7dd97bb3335
CrioUser <crio-user@criodo.com>
2021-09-15 18:07:35 +0530

    Try to access MEME with some random id, and verify that it returns 404

--- src/main/java/com/crio/starter/service/GreetingsService.java
+++ src/main/java/com/crio/starter/service/GreetingsService.java
@@ -35,6 +35,9 @@ public class GreetingsService {
   }
 
 
+  public GreetingsEntity findById(long id) {
+    return greetingsRepository.findById(id);
+  }
   public GreetingsEntity findByName(String name) {
     return greetingsRepository.findByName(name);
   }
