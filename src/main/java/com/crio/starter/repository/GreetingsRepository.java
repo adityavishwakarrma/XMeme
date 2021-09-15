f6cf52517d02be391565bf28eceb978d9ab84704
Aditya Vishwakarma <adityakumaarvishwakarma@gmail.com>
2021-09-15 18:50:51 +0000

    Update GreetingsRepository.java

--- src/main/java/com/crio/starter/repository/GreetingsRepository.java
+++ src/main/java/com/crio/starter/repository/GreetingsRepository.java
@@ -1,15 +1,8 @@
 package com.crio.starter.repository;
 
-import java.util.List;
-
 import com.crio.starter.data.GreetingsEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface GreetingsRepository extends MongoRepository<GreetingsEntity, Long> {
-  <S extends GreetingsEntity> S save(S entity);
-  List<GreetingsEntity> findAll();
-  GreetingsEntity findById(long id);
-  GreetingsEntity findByName(String name);
-  GreetingsEntity findByUrl(String url); 
-  GreetingsEntity findByCaption(String caption);
+public interface GreetingsRepository extends MongoRepository<GreetingsEntity, String> {
+  GreetingsEntity findByExtId(String extId);
 }
