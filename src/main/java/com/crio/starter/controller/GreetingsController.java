4c98d38fcff5b08d2fc54694e4c61e964e756953
CrioUser <crio-user@criodo.com>
2021-09-15 15:43:06 +0530

    starter template

--- src/main/java/com/crio/starter/controller/GreetingsController.java
+++ src/main/java/com/crio/starter/controller/GreetingsController.java
@@ -1,21 +1,10 @@
 package com.crio.starter.controller;
 
-import com.crio.starter.data.GreetingsEntity;
 import com.crio.starter.exchange.ResponseDto;
 import com.crio.starter.service.GreetingsService;
-import com.rabbitmq.client.RpcClient.Response;
-
 import lombok.RequiredArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,53 +15,8 @@ public class GreetingsController {
   private final GreetingsService greetingsService;
 
   @GetMapping("/say-hello")
-  public ResponseDto sayHello(@RequestParam Long messageId) {
-    return greetingsService.getMessage(messageId);
+  public ResponseDto sayHello(@RequestParam long id) {
+    return greetingsService.getMessage(id);
   }
-
-  @PostMapping("/memes/")//test 2 pass
-  public ResponseEntity<ResponseDto> postMeme(@RequestBody GreetingsEntity greetingsEntity) {
-
-    String name = greetingsEntity.getName();
-    String url =  greetingsEntity.getUrl();
-    String caption = greetingsEntity.getCaption();
-
-    if(name==null && url==null && caption==null) { //test 6 FAIL
-      return ResponseEntity.badRequest().body(null);
-    }
-
-    if (greetingsService.findByName(name) != null || greetingsService.findByUrl(url) != null
-        || greetingsService.findByCaption(caption) != null) {
-      return ResponseEntity.status(409).body(null);  //TEST 5
-
-    } else {
-      ResponseDto response = greetingsService.postMeme(greetingsEntity);
-      return ResponseEntity.ok().body(response);
-    }
-  }
-
-  @GetMapping("/memes/") //test 1 pass 
-                         //test 7 fail
-                         //test 8 pass
-  public ResponseEntity<List<GreetingsEntity>> getMemes(){
-    List<GreetingsEntity> greetingsEntities = new ArrayList<>();
-    greetingsEntities = greetingsService.getMemes();
-    return ResponseEntity.ok().body(greetingsEntities); 
-  }
-
-  // @GetMapping("/memes/{id}") //test 3 FAIL 4
-  // public ResponseEntity<GreetingsEntity> getMeme(@PathVariable("id") Long id) {
-  //   GreetingsEntity greetingsEntity = greetingsService.findById(id);
-  //   if(greetingsEntity != null) {
-  //   return ResponseEntity.ok(greetingsEntity);
-  //   } else {
-  //   return ResponseEntity.ok(null);
-  //   }
-  // }
 
 }
-
-
-
-
-
--- src/main/java/com/crio/starter/service/GreetingsService.java
+++ src/main/java/com/crio/starter/service/GreetingsService.java
@@ -1,59 +1,17 @@
 package com.crio.starter.service;
 
-import com.crio.starter.data.GreetingsEntity;
 import com.crio.starter.exchange.ResponseDto;
 import com.crio.starter.repository.GreetingsRepository;
 import lombok.RequiredArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@RequiredArgsConstructor
 public class GreetingsService {
 
   private final GreetingsRepository greetingsRepository;
 
-  public ResponseDto getMessage(Long id) {
-    return new ResponseDto(id);
-  }
-
-  private final AtomicLong counter = new AtomicLong();
-  
-  public ResponseDto postMeme(GreetingsEntity greetingsEntity) {
-    greetingsEntity.setId(counter.incrementAndGet());
-    greetingsRepository.save(greetingsEntity);
-    return new ResponseDto(greetingsEntity.getId());
-  }
-
-  public List<GreetingsEntity> getMemes(){
-    List<GreetingsEntity> greetingsEntities = greetingsRepository.findAll();
-    if(greetingsEntities.size() <=100){
-      return greetingsEntities;             //TEST 7
-    } else {
-      List<GreetingsEntity> list = new ArrayList<>();
-      for(int i=0; i<100; i++){             //TEST 8 FAIL
-        list.add(greetingsEntities.get(i));
-      }
-      return list;
-    }
-  }
-
-
-  public GreetingsEntity findById(long id){
-    return greetingsRepository.findById(id);
-  }
-  public GreetingsEntity findByName(String name){
-    return greetingsRepository.findByName(name);
-  }
-  public GreetingsEntity findByUrl(String url){
-    return greetingsRepository.findByUrl(url);
-  }
-  public GreetingsEntity findByCaption(String caption){
-    return greetingsRepository.findByCaption(caption);
+  public ResponseDto getMessage(long id) {
+    return new ResponseDto(greetingsRepository.findById(id).getId());
   }
 }
